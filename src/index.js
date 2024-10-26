c165c5c9938a4c6af1e5f9044dcf95e09ed77ce8
Saim Nomani <muhammadsaimnomani951@gmail.com>
2024-10-26 21:18:30 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+import App from './App';
+// import StarRating from "./StarRating";
+
+// const Test = function () {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <>
+//       <StarRating color="blue" onSetMovieRating={setMovieRating}/>
+//       <p>The movie was rated {movieRating} stars.</p>
+//     </>
+//   );
+// };
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      size={32}
+      className="test"
+      color="yellow"
+      messages={["terrible", "bed", "okey", "good", "amazing"]}
+      defaultRating={3}
+    />
+    <StarRating />
+    <Test/> */}
+  </React.StrictMode>
+);
